b27002e3eda6d7845470af415cf76c1f9d1824be
Yan Tarasov <vzu3427@gmail.com>
2025-12-03 15:51:17 +0200

    Added CW2

--- CW_1_CS/Program.cs
+++ CW_1_CS/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace CW_1_CS;
+﻿//namespace CW_1_CS;
 
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Enter number 1: ");
+//        int n1 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("Enter number 2: ");
+//        int n2 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("Enter number 3: ");
+//        int n3 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("Enter number 4: ");
+//        int n4 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("Enter number 5: ");
+//        int n5 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("Enter number 6: ");
+//        int n6 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine($"Original: {n1}, {n2}, {n3}, {n4}, {n5}, {n6}");
+//        Console.WriteLine($"Reversed: {n6}, {n5}, {n4}, {n3}, {n2}, {n1}");
+//    }
+//}
